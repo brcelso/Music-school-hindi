273a9b7bfead28239b571bef35f5f9041e541fc0
celso <celso.sjunior@outlook.com>
2024-04-08 12:22:19 +0000

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: ['github.com']
+        domains: ['github.com']
         //domains: ['images.unsplash.com', 'res.cloudinary.com']
     }
 };
